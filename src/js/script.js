a09dc5e146a9633e05ef9ab32ff7d0c1f1df79a4
ilya <patlatov@inbox.ru>
2022-03-07 23:00:41 +0300

    fixfix

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,29 @@
 // Write JS code here
-'use strict'
-
 $('.programs-workouts__row').slick({
-  infinite: true, 
-  slidesToShow: 2,
-  slidesToScroll: 1
-  });
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        arrow: false,
+        dots: true,
+
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    },
+  ]
+});
